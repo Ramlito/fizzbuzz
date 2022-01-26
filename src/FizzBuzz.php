7cb616f2f7a21e215047ddfe6a0649ccbccaf203
Ramlito <thbltmrl@gmail.com>
2022-01-26 14:09:03 +0100

    modifs test

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -8,14 +8,16 @@ class FizzBuzz
 
         if ($value % 3 == 0) {
             $returnValue =  'Fizz';
+            return $returnValue;
         }
         elseif ($value % 5 == 0) {
             $returnValue = 'Buzz';
+            return $returnValue;
         }
         elseif ($value == 0 % 5 && $value % 3 == 0) {
             $returnValue = 'FizzBuzz';
+            return $returnValue;
         }
 
-        return $returnValue;
     }
 }
